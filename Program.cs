ae2b5bb19d943b63b976330a42f21bf75a544b92
stuartjshepherd <stuart@createstudios.com.au>
2020-01-22 16:19:40 +1100

    removed test code

--- Program.cs
+++ Program.cs
@@ -9,7 +9,6 @@ namespace DateTimeHelper
             Console.WriteLine("DateTime Helper");
             Console.WriteLine("Global class providing DateTime methods.");
             Console.WriteLine();
-            Console.WriteLine(DateTimeHelper.IsValidFromTo(new DateTime(2020, 2, 2), new DateTime(2020, 2, 2)));
         }
     }
 }
